707672c477150b0f64494c16764f06f5ac837448
Silverlane <Silverlane@MARKUS-PC>
2022-10-19 14:05:36 +0200

    Projektdateien hinzufügen.

---
+++ App.xaml.cs
@@ -0,0 +1,11 @@
+﻿namespace Delta;
+
+public partial class App : Application
+{
+	public App()
+	{
+		InitializeComponent();
+
+		MainPage = new AppShell();
+	}
+}
---
+++ AppShell.xaml.cs
@@ -0,0 +1,9 @@
+﻿namespace Delta;
+
+public partial class AppShell : Shell
+{
+	public AppShell()
+	{
+		InitializeComponent();
+	}
+}
